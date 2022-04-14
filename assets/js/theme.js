e7ec2fc331350986b619577a566c9c99690e5722
MentalCalculator <deniss-ozerskis@inbox.lv>
2022-04-14 22:32:08 +0300

    4.diena

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,9 @@
 let introductionVideo = document.getElementById("introductionVideo");
 let playButton = document.getElementById("playButton");
+let videoSection = document.getElementsByClassName("video-section")[0];
 let playingVideo = 0;
 
-introductionVideo.addEventListener("onclick", playVideo);
+videoSection.addEventListener("click", playVideo);
 
 function playVideo () {
     if(playingVideo == 0){
@@ -17,4 +18,30 @@ function playVideo () {
     }
 }
 
-console.log(playingVideo);
+
+
+  
+
+let dropdownOpenButton = document.getElementById("hamburgerOpen");
+let dropdownCloseButton = document.getElementById("hamburgerClose");
+let navigationList = document.getElementsByClassName("navigation-list")[0];
+
+
+dropdownOpenButton.addEventListener("click", openMenu);
+
+function openMenu () {
+    navigationList.classList.add("opened");
+    dropdownOpenButton.style.display = "none";
+    dropdownCloseButton.style.display = "block";
+}
+
+dropdownCloseButton.addEventListener("click", closeMenu);
+
+function closeMenu () {
+    navigationList.classList.remove("opened");
+    dropdownOpenButton.style.display = "block";
+    dropdownCloseButton.style.display = "none";
+}
+
+
+
